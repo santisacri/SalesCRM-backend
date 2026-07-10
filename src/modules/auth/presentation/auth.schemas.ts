6ecb5f83066b6145c0b0236ee501b6cb8abc2f60
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-07-09 21:54:02 -0300

    feat: add register user schema

---
+++ src/modules/auth/presentation/auth.schemas.ts
@@ -0,0 +1,24 @@
+import z from "zod";
+
+const passwordValidation = z
+    .string()
+    .min(8, { error: 'Password must be at least 8 characters long' })
+    .max(24, { error: 'Password must be at most 24 characters long' })
+    .regex(/[0-9]/, { error: 'Password must contain at least one number' })
+    .regex(/[a-z]/, { error: 'Password must contain at least one lowercase letter' })
+    .regex(/[A-Z]/, { error: 'Password must contain at least one uppercase letter' });
+
+const nameValidation = z
+    .string()
+    .trim()
+    .min(2, { error: 'At least 2 characters long' })
+    .max(50, { error: 'Max 50 characters long' })
+    .regex(/^[\p{L}\s'-]+$/u, { error: 'Name cannot contain special characters' });
+
+export const registerUserSchema = z.object({
+    name: nameValidation,
+    password: passwordValidation,
+    email: z.email(),
+})
+
+export type TRegisterUser = z.infer<typeof registerUserSchema>
