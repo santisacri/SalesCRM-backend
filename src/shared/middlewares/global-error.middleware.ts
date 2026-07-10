d26af902df4189ffd8c98a70d7d5ec4dc45c77b8
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-07-10 18:32:46 -0300

    fix: prevent double responses and log custom error on dev env

--- src/shared/middlewares/global-error.middleware.ts
+++ src/shared/middlewares/global-error.middleware.ts
@@ -5,17 +5,18 @@ import envs from "../config/envs"
 const errorLog = (error: CustomError) => {
     return `CustomError = {  
     message: ${error.message},  
-    stack: ${error.stack},
+    stack: ${error.stack?.split('SalesCRM-Backend').at(-1)},
     date: ${new Date(Date.now()).toLocaleString('es-AR')},
 }`
 }
 
 const errorMiddleware = (err: unknown, _req: Request, res: Response, _next: NextFunction) => {
     if (err instanceof CustomError) {
-        if (!envs.IN_PRODUCTION) errorLog(err)
+        if (!envs.IN_PRODUCTION) console.log(errorLog(err))
         res.status(err.statusCode).json({
             message: err.message
         })
+        return
     }
 
     console.log(`Unexpected Error: ${err}`)
