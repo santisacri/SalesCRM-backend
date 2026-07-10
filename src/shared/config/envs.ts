48ac6e39302101c23daca4aee9bca7e77725e8d7
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-07-09 21:58:21 -0300

    chore: add .env.example and IN_PRODUCTION on envs

--- src/shared/config/envs.ts
+++ src/shared/config/envs.ts
@@ -3,6 +3,7 @@ import env from 'env-var'
 
 const envs = {
     PORT: env.get('PORT').required().asPortNumber(),
+    IN_PRODUCTION: env.get('IN_PRODUCTION').required().asBool(),
     DB_USER: env.get('DB_USER').required().asString(),
     DB_PASSWORD: env.get('DB_PASSWORD').required().asString(),
     DB_NAME: env.get('DB_NAME').required().asString(),
