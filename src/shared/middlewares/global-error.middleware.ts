5728418c705b6703b19fbbf27f93c9550ebe614e
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-07-20 17:51:11 -0300

    refactor: print raw custom error

--- src/shared/middlewares/global-error.middleware.ts
+++ src/shared/middlewares/global-error.middleware.ts
@@ -2,17 +2,17 @@ import { NextFunction, Request, Response } from "express"
 import { CustomError } from "../errors/custom-errors"
 import envs from "../config/envs"
 
-const errorLog = (error: CustomError) => {
-    return `CustomError = {  
-    message: ${error.message},  
-    stack: ${error.stack?.split('SalesCRM-Backend').at(-1)},
-    date: ${new Date(Date.now()).toLocaleString('es-AR')},
-}`
-}
+// const errorLog = (error: CustomError) => {
+//     return `CustomError = {  
+//     message: ${error.message},  
+//     stack: ${error.stack?.split('SalesCRM-Backend').at(-1)},
+//     date: ${new Date(Date.now()).toLocaleString('es-AR')},
+// }`
+// }
 
 const errorMiddleware = (err: unknown, _req: Request, res: Response, _next: NextFunction) => {
     if (err instanceof CustomError) {
-        if (!envs.IN_PRODUCTION) console.log(errorLog(err))
+        if (!envs.IN_PRODUCTION) console.log(err)
         res.status(err.statusCode).json({
             message: err.message
         })
