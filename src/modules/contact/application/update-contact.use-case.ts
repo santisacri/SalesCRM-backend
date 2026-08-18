738b91d41a28e611fd065351c726f83072cdcb2a
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-08-18 12:03:20 -0300

    feat: add updateContact use case

---
+++ src/modules/contact/application/update-contact.use-case.ts
@@ -0,0 +1,31 @@
+import { CustomError } from "../../../shared/errors/custom-errors";
+import { ErrorCode } from "../../../shared/errors/error-codes";
+import { Ctx } from "../../../shared/types/context.types";
+import { MembershipRoleEnum } from "../../membership/domain/membership.entity";
+import { ContactEntity } from "../domain/contact.entity";
+import { IContactRepository } from "../domain/contact.repository.contract";
+import { UpdateContactInput } from "../presentation/contact.schemas";
+
+export interface IUpdateContactUseCase {
+    execute(input: UpdateContactInput, contactId: string, ctx: Ctx): Promise<ContactEntity>
+}
+
+export class UpdateContactUseCase implements IUpdateContactUseCase {
+
+    constructor(
+        private readonly contactRepo: IContactRepository,
+    ) { }
+
+    async execute(input: UpdateContactInput, contactId: string, ctx: Ctx): Promise<ContactEntity> {
+        const storedContact = await this.contactRepo.findById(contactId, ctx.organizationId)
+
+        if (!storedContact) throw CustomError.notFound('Contact not found', ErrorCode.NOT_FOUND);
+
+        if (ctx.role === MembershipRoleEnum.MEMBER && storedContact.ownerId !== ctx.userId) throw CustomError.forbidden("You can't update this contact", ErrorCode.FORBIDDEN);
+
+        const updatedContact = await this.contactRepo.update(input, contactId, ctx.organizationId)
+
+        return updatedContact
+    }
+
+}
