185c89f909e9984f40fc23204ec29dd4281b4d08
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-08-18 11:58:49 -0300

    fix: remove bad logic that let the request continue when zod throwed and return a generic message when not in production

--- src/shared/middlewares/validate-body.middleware.ts
+++ src/shared/middlewares/validate-body.middleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express"
 import z, { ZodType } from "zod"
 import { ErrorCode } from "../errors/error-codes"
+import { CustomError } from "../errors/custom-errors"
+import envs from "../config/envs"
 
 const validateBody = (schema: ZodType) => {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -8,15 +10,21 @@ const validateBody = (schema: ZodType) => {
         const { success, error, data } = schema.safeParse(req.body)
 
         if (!success) {
+            if (envs.IN_PRODUCTION) {
+                throw CustomError.badRequest('Invalid input', ErrorCode.VALIDATION_ERROR)
+            }
+
             res.status(400).json({
                 error: z.flattenError(error).fieldErrors,
                 code: ErrorCode.VALIDATION_ERROR
             })
+
+            return
+        } else {
+            req.body = data
+
+            next()
         }
-
-        req.body = data
-
-        next()
     }
 }
 
