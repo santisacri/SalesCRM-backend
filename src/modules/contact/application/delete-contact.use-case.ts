0fa342ab6fb7efc7b140dc55d46c3b27a5fe13c0
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-08-18 14:59:36 -0300

    feat: add delete-contact use case

---
+++ src/modules/contact/application/delete-contact.use-case.ts
@@ -0,0 +1,29 @@
+import { CustomError } from "../../../shared/errors/custom-errors";
+import { ErrorCode } from "../../../shared/errors/error-codes";
+import { Ctx } from "../../../shared/types/context.types";
+import { MembershipRoleEnum } from "../../membership/domain/membership.entity";
+import { IContactRepository } from "../domain/contact.repository.contract";
+
+export interface IDeleteContactUseCase {
+    execute(contactId: string, ctx: Ctx): Promise<void>
+}
+
+export class DeleteContactUseCase implements IDeleteContactUseCase {
+
+    constructor(
+        private readonly contactRepo: IContactRepository
+    ) { }
+
+    async execute(contactId: string, ctx: Ctx): Promise<void> {
+        const storedContact = await this.contactRepo.findById(contactId, ctx.organizationId)
+
+        if (!storedContact) throw CustomError.notFound('Contact not found', ErrorCode.NOT_FOUND);
+
+        if (ctx.role === MembershipRoleEnum.MEMBER && storedContact.ownerId !== ctx.userId) {
+            throw CustomError.forbidden("You can't delete this contact", ErrorCode.FORBIDDEN);
+        }
+
+        await this.contactRepo.deleteById(contactId, ctx.organizationId)
+    }
+
+}
