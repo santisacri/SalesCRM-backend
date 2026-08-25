c3afd4e148c8e405b473a8a8c3fd745ce905c2b4
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-08-24 21:56:41 -0300

    feat: add create-team use case

---
+++ src/modules/team/application/create-team.use-case.ts
@@ -0,0 +1,32 @@
+import { CustomError } from "../../../shared/errors/custom-errors";
+import { OrgScopedCtx } from "../../../shared/types/context.types";
+import { MembershipRoleEnum } from "../../membership/domain/membership.entity";
+import { IMembershipRepository } from "../../membership/domain/membership.repository.contract";
+import { TeamEntity } from "../domain/team.entity";
+import { ITeamRepository } from "../domain/team.repository.contract";
+import { CreateTeamInput } from "../presentation/team.schemas";
+
+export interface ICreateTeamUseCase {
+    execute(input: CreateTeamInput, ctx: OrgScopedCtx): Promise<TeamEntity>
+}
+
+export class CreateTeamUseCase implements ICreateTeamUseCase {
+
+    constructor(
+        private readonly teamRepo: ITeamRepository,
+        private readonly membershipRepo: IMembershipRepository
+    ) { }
+
+    async execute(input: CreateTeamInput, ctx: OrgScopedCtx): Promise<TeamEntity> {
+        const member = await this.membershipRepo.findActive(input.adminId, ctx.organizationId)
+
+        if(!member) throw CustomError.notFound("Member not found");
+
+        if(member.role === MembershipRoleEnum.OWNER) throw CustomError.badRequest();
+
+        if(member.role === MembershipRoleEnum.ADMIN) throw CustomError.badRequest("A member can only lead one team at a time");
+
+        return this.teamRepo.create(input.name, input.adminId, ctx.organizationId)
+    }
+
+}
