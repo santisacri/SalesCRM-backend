dc8c0cc38637bd226dbe214b09e544d19d18d9fc
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-08-27 14:55:21 -0300

    refactor: change invitation repo interface and fix naming

--- src/modules/invitation/domain/invitation.repository.contract.ts
+++ src/modules/invitation/domain/invitation.repository.contract.ts
@@ -2,7 +2,7 @@ import { OrgScopedCtx } from "../../../shared/types/context.types";
 import { InvitationEntity } from "./invitation.entity";
 
 export interface IInvitationRepository {
-    create(email: string, token: string, invitedByCtx: OrgScopedCtx): Promise<InvitationEntity>
+    create(email: string, invitedByCtx: OrgScopedCtx): Promise<{dbRecord: InvitationEntity, hashedToken: string}>
     findByToken(token: string): Promise<InvitationEntity | null>
     listByOrg(organizationId: string): Promise<InvitationEntity[]>
     update(invitation: InvitationEntity): Promise<InvitationEntity>
