e0ed6f7cbe3dbe97b70b966146ee1954038a269a
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-08-27 16:00:44 -0300

    feat: add INVITATION_EXP_MS constant

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -3,6 +3,7 @@ import envs from "./envs";
 
 export const ACCESS_TOKEN_EXP = envs.IN_PRODUCTION ? 60 * 15 : 60 * 60
 export const REFRESH_TOKEN_EXP = 60 * 60 * 24 * 7
+export const INVITATION_EXPIRATION_MS = 1000 * 60 * 60 * 24 * 5
 
 export const TOKEN_EXPIRATION_MS: Record<TokenType, number> = {
     [TokenType.EMAIL_VERIFICATION]: 24 * 60 * 60 * 1000,
