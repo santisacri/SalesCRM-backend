843aad8a355eb3c62d6a41562c8d8d54b7d0d056
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-08-27 16:03:20 -0300

    feat: add invite-to-organization use case

---
+++ src/modules/invitation/application/invite-to-organization.use-case.ts
@@ -0,0 +1,39 @@
+import { IMailQueueService } from "../../../shared/queue/mail/mail-queue.service.contract";
+import { OrgScopedCtx } from "../../../shared/types/context.types";
+import { IOrganizationRepository } from "../../organization/domain/organization.repository.contract";
+import { IUserRepository } from "../../user/domain/user.repository.contract";
+import { IInvitationRepository } from "../domain/invitation.repository.contract";
+import { InvitationEntity } from "../domain/invitation.entity";
+
+export interface IInviteToOrganizationUseCase {
+    execute(email: string, userCtx: OrgScopedCtx): Promise<InvitationEntity>
+}
+
+export class InviteToOrganizationUseCase implements IInviteToOrganizationUseCase {
+
+    constructor(
+        private readonly invitationRepo: IInvitationRepository,
+        private readonly userRepo: IUserRepository,
+        private readonly organizationRepo: IOrganizationRepository,
+        private readonly mailQueue: IMailQueueService
+    ) { }
+
+    async execute(email: string, userCtx: OrgScopedCtx): Promise<InvitationEntity> {
+
+        const [{ dbRecord, hashedToken }, user, organization] = await Promise.all([
+            await this.invitationRepo.create(email, userCtx),
+            await this.userRepo.getById(userCtx.userId),
+            await this.organizationRepo.getById(userCtx.organizationId)
+        ])
+
+        await this.mailQueue.enqueue("send-invitation-email", {
+            to: email,
+            invitedByName: user.name,
+            organizationName: organization.name,
+            hashedToken
+        })
+
+        return dbRecord
+    }
+
+}
