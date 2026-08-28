fa934230afd3e25d41a0109852376aaeb160824a
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-08-27 21:58:43 -0300

    refactor: rename invitation route prefix to /api/invitations

--- src/app.router.ts
+++ src/app.router.ts
@@ -14,7 +14,7 @@ export class AppRouter {
         router.use('/api/org', OrganizationRouter.routes)
         router.use('/api/contact', ContactRouter.routes)
         router.use('/api/team', TeamRouter.routes)
-        router.use('/api/invite', InvitationRouter.routes)
+        router.use('/api/invitations', InvitationRouter.routes)
 
         return router
     }
