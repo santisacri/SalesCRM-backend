80dee31fbe1489066365d5909a20afd93dc0fba4
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-09-10 17:16:43 -0300

    fix: typo

--- src/modules/membership/application/get-organization-members.use-case.ts
+++ src/modules/membership/application/get-organization-members.use-case.ts
@@ -1,9 +1,9 @@
-import { MembershipWithName } from "../domain/membership.datasource.contract";
+import { MembershipWithUser } from "../domain/membership.datasource.contract";
 import { MembershipStatusEnum } from "../domain/membership.entity";
 import { IMembershipRepository } from "../domain/membership.repository.contract";
 
 export interface IGetOrganizationMembersUseCase {
-    execute(organizationId: string, status: string): Promise<MembershipWithName[]>
+    execute(organizationId: string, status: string): Promise<MembershipWithUser[]>
 }
 
 export class GetOrganizationMembersUseCase implements IGetOrganizationMembersUseCase {
@@ -12,7 +12,7 @@ export class GetOrganizationMembersUseCase implements IGetOrganizationMembersUse
         private readonly membershipRepo: IMembershipRepository
     ) { }
 
-    async execute(organizationId: string, status: string): Promise<MembershipWithName[]> {
+    async execute(organizationId: string, status: string): Promise<MembershipWithUser[]> {
         return this.membershipRepo.findManyByOrg(organizationId, status as MembershipStatusEnum)
     }
 
