e2e46dcc9b631fbcb927c6aed97ec39dd6a5f58b
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-09-15 23:07:47 -0300

    refactor: put team creation inside a transaction and also add missing admin's membership update

--- src/modules/team/application/create-team.use-case.ts
+++ src/modules/team/application/create-team.use-case.ts
@@ -1,3 +1,4 @@
+import { ITransactionManager } from "../../../shared/database/transaction-manager";
 import { CustomError } from "../../../shared/errors/custom-errors";
 import { OrgScopedCtx } from "../../../shared/types/context.types";
 import { MembershipRoleEnum } from "../../membership/domain/membership.entity";
@@ -14,19 +15,29 @@ export class CreateTeamUseCase implements ICreateTeamUseCase {
 
     constructor(
         private readonly teamRepo: ITeamRepository,
-        private readonly membershipRepo: IMembershipRepository
+        private readonly membershipRepo: IMembershipRepository,
+        private readonly txManager: ITransactionManager
     ) { }
 
     async execute(input: CreateTeamInput, ctx: OrgScopedCtx): Promise<TeamEntity> {
         const member = await this.membershipRepo.findActive(input.adminId, ctx.organizationId)
 
-        if(!member) throw CustomError.notFound("Member not found");
+        if (!member) throw CustomError.notFound("Member not found");
 
-        if(member.role === MembershipRoleEnum.OWNER) throw CustomError.badRequest();
+        if (member.role === MembershipRoleEnum.OWNER) throw CustomError.badRequest();
 
-        if(member.role === MembershipRoleEnum.ADMIN) throw CustomError.badRequest("A member can only lead one team at a time");
+        if (member.role === MembershipRoleEnum.ADMIN) throw CustomError.badRequest("A member can only lead one team at a time");
 
-        return this.teamRepo.create(input.name, input.adminId, ctx.organizationId)
+
+        const team = this.txManager.run(async (tx) => {
+            let team = await this.teamRepo.create(input.name, input.adminId, ctx.organizationId, tx)
+
+            await this.membershipRepo.update({ ...member, role: MembershipRoleEnum.ADMIN, teamId: team.id }, tx)
+
+            return team
+        })
+
+        return team
     }
 
 }
--- src/shared/container/team.container.ts
+++ src/shared/container/team.container.ts
@@ -2,10 +2,11 @@ import { CreateTeamUseCase } from "../../modules/team/application/create-team.us
 import { ListOrganizationTeamsUseCase } from "../../modules/team/application/list-organization-teams.use-case";
 import { TeamController } from "../../modules/team/presentation/team.controller";
 import { membershipRepository, teamRepository } from "./repositories.container";
+import { transactionManager } from "./transaction-manager.container";
 
 
 
-const createTeam = new CreateTeamUseCase(teamRepository, membershipRepository)
+const createTeam = new CreateTeamUseCase(teamRepository, membershipRepository, transactionManager)
 const listOrganizationTeams = new ListOrganizationTeamsUseCase(teamRepository)
 
 export const teamController = new TeamController({
