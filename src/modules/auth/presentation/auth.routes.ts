8dd53f1e809cabcf1e6a290a218b2981d5a7acaa
Santiago Sacripanti <santisacripanti1@gmail.com>
2026-09-16 21:01:53 -0300

    feat: add turnstile middleware on login and register routes

--- src/modules/auth/presentation/auth.routes.ts
+++ src/modules/auth/presentation/auth.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import validateBody from "../../../shared/middlewares/validate-body.middleware";
 import { forgotPasswordSchema, loginUserSchema, registerUserSchema, resetPasswordSchema, selectOrgSchema, verifyEmailSchema } from "./auth.schemas";
-import { authController, authMiddleware } from "../../../shared/container/auth.container";
+import { authController, authMiddleware, turnstileMiddleware } from "../../../shared/container/auth.container";
 
 
 export class AuthRouter {
     static get routes(): Router {
         const router = Router()
 
-        router.post('/register', [validateBody(registerUserSchema)], authController.register)
-        router.post('/login', [validateBody(loginUserSchema)], authController.login)
+        router.post('/register', [turnstileMiddleware, validateBody(registerUserSchema)], authController.register)
+        router.post('/login', [turnstileMiddleware, validateBody(loginUserSchema)], authController.login)
         router.get('/refresh', authController.refresh)
         router.post('/forgot-password', [validateBody(forgotPasswordSchema)], authController.forgotPassword)
         router.post('/reset-password', [validateBody(resetPasswordSchema)], authController.resetPassword)
--- src/shared/container/auth.container.ts
+++ src/shared/container/auth.container.ts
@@ -8,11 +8,13 @@ import { VerifyEmailUseCase } from "../../modules/auth/application/verify-email.
 import { AuthController } from "../../modules/auth/presentation/auth.controller";
 import { SelectOrganizationUseCase } from "../../modules/membership/application/select-organization.use-case";
 import { AuthMiddleware } from "../middlewares/auth.middleware";
+import verifyTurnstileMiddleware from "../middlewares/verify-turnstile.middleware";
 import { mailQueueService } from "./queue.container";
 import { membershipRepository, refreshTokenRepository, tokenRepository, userRepository } from "./repositories.container";
-import { hashService, jwtService } from "./services.container";
+import { hashService, jwtService, turnstileService } from "./services.container";
 
 export const authMiddleware = new AuthMiddleware(userRepository, membershipRepository, jwtService).validate
+export const turnstileMiddleware = new verifyTurnstileMiddleware(turnstileService).verify
 
 const registerUser = new RegisterUserUseCase(userRepository, tokenRepository, hashService, mailQueueService)
 const loginUser = new LoginUserUseCase(userRepository, refreshTokenRepository, hashService, jwtService)
